aef96537fde2e0dae7f0c35a738b00364d2af20b
noureqo <128331928+noureqo@users.noreply.github.com>
2024-12-05 10:23:50 +0200

    Signup , login ,Index .html pages with bootstrap and css #17

--- src/main/java/BananaBrain/BananaBrain/BananaBrainApplication.java
+++ src/main/java/BananaBrain/BananaBrain/BananaBrainApplication.java
@@ -2,8 +2,6 @@ package BananaBrain.BananaBrain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BananaBrainApplication {
@@ -12,13 +10,4 @@ public class BananaBrainApplication {
 		SpringApplication.run(BananaBrainApplication.class, args);
 	}
 
-	// REST Controller to handle web requests
-	@RestController
-	class HelloWorldController {
-
-		@GetMapping("/")
-		public String helloWorld() {
-			return "Hello, Noureqo!";
-		}
-	}
 }
---
+++ src/main/java/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(httpForm ->{
+                    httpForm.loginPage("/req/login").permitAll();
+                    httpForm.defaultSuccessUrl("/index");
+
+                })
+
+
+                .authorizeHttpRequests(registry ->{
+                    registry.requestMatchers("/req/signup","/css/**","/js/**").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+}
+
