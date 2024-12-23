bc26c917a92790e2663c7ef007d2163ad47b00ab
darkhyper24 <s-mohammed.elmaadawy@zewailcity.edu.eg>
2024-12-23 04:10:54 +0200

    changed the roles relation with the user to one to one

--- src/main/java/BananaBrain/model/Roles.java
+++ src/main/java/BananaBrain/model/Roles.java
@@ -4,26 +4,24 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-
-@Table(name = "roles")
 @Entity
+@Table(name = "roles")
 public class Roles {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Setter
     @Getter
     private int id;
+
     @Setter
     @Getter
     @Column(nullable = false)
     private String role;
 
-    @ManyToMany(mappedBy = "roles", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    private Set<MyAppUser> users = new HashSet<>();
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "user_id")
+    private MyAppUser user;
 
     // Constructor
     public Roles() {}
@@ -32,12 +30,12 @@ public class Roles {
         this.role = role;
     }
 
-    // Getter for users
-    public Set<MyAppUser> getUsers() {
-        return users;
+    // Getter for user
+    public MyAppUser getUser() {
+        return user;
     }
 
-    public void setUsers(Set<MyAppUser> users) {
-        this.users = users;
+    public void setUser(MyAppUser user) {
+        this.user = user;
     }
 }
